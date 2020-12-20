3851a1216958fd59a56c8f928342a524348c8767
vince <vince@air.local>
2020-12-19 18:34:33 -0800

    bump ver and changelog

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ def loader():
 class MySkinInstaller(ExtensionInstaller):
     def __init__(self):
         super(MySkinInstaller, self).__init__(
-            version="0.1",
+            version="0.2",
             name='ecowitt',
             description='ecowitt minimalist custom skin',
             author="Vince Skahan",
