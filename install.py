b0d7c3c090cfa5eb656f80034794db3ebf537667
vince <vince@air.local>
2021-08-10 21:39:59 -0700

    update to 0.4

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ def loader():
 class MySkinInstaller(ExtensionInstaller):
     def __init__(self):
         super(MySkinInstaller, self).__init__(
-            version="0.3",
+            version="0.4",
             name='ecowitt',
             description='ecowitt minimalist custom skin',
             author="Vince Skahan",
