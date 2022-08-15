9f62b12cf56c9554fd63a05c4933001d11a2cc82
Vince Skahan <vinceskahan@gmail.com>
2022-08-15 11:08:10 -0700

    update to 5 sensors

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ def loader():
 class MySkinInstaller(ExtensionInstaller):
     def __init__(self):
         super(MySkinInstaller, self).__init__(
-            version="0.4",
+            version="0.5",
             name='ecowitt',
             description='ecowitt minimalist custom skin',
             author="Vince Skahan",
@@ -28,7 +28,10 @@ class MySkinInstaller(ExtensionInstaller):
                     'field_map_extensions': {
                         'outTempBatteryStatus': 'wh26_batt',
                         'batteryStatus1': 'wh31_ch1__batt',
-                        'batteryStatus2': 'wh31_ch1__batt',
+                        'batteryStatus2': 'wh31_ch2__batt',
+                        'batteryStatus3': 'wh31_ch3__batt',
+                        'batteryStatus4': 'wh31_ch4__batt',
+                        'batteryStatus5': 'wh31_ch5__batt',
                         'batteryStatus8': 'wh51_ch1__batt',
                     }
                 }
